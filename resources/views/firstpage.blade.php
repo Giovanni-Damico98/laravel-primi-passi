0fa83d0270791acb5c58a6f858927a0f931e9e36
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-10-23 01:17:58 +0300

    views and route completed, exercise and bonus completed

---
+++ resources/views/firstpage.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <button class="btn btn-primary"><a class="nav-link fw-bold" href="{{ route('home') }}">Home</a></button>
+            </li>
+        </ul>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center mt-4 mb-5">
+                    {{ $titolo }}
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            @foreach ($pokemonArray as $pokemonItem)
+            <div class="col-md-4"> <!-- Bootstrap col-4 per avere 3 card per riga -->
+                <div class="card mb-4">
+                    <img src="{{ $pokemonItem['img_url'] }}" class="card-img-top" alt="{{ $pokemonItem['name'] }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $pokemonItem['name'] }}</h5>
+                        <p class="card-text">
+                            <strong>Type:</strong> {{ $pokemonItem['type'] }} <br>
+                            <strong>Level:</strong> {{ $pokemonItem['level'] }} <br>
+                            <strong>HP:</strong> {{ $pokemonItem['hp'] }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,151 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Rotta per la home
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+// Rotta per la prima pagina
+Route::get('/firstpage', function () {
+    $titolo = 'Pokedex';
+    $pokemonArray = [
+        [
+            'name' => 'Pikachu',
+            'type' => 'Electric',
+            'level' => 25,
+            'hp' => 90,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
+        ],
+        [
+            'name' => 'Charmander',
+            'type' => 'Fire',
+            'level' => 16,
+            'hp' => 80,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+        ],
+        [
+            'name' => 'Bulbasaur',
+            'type' => 'Grass/Poison',
+            'level' => 12,
+            'hp' => 85,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+        ],
+        [
+            'name' => 'Squirtle',
+            'type' => 'Water',
+            'level' => 18,
+            'hp' => 88,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+        ],
+        [
+            'name' => 'Jigglypuff',
+            'type' => 'Normal/Fairy',
+            'level' => 14,
+            'hp' => 100,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png'
+        ],
+        [
+            'name' => 'Gengar',
+            'type' => 'Ghost/Poison',
+            'level' => 40,
+            'hp' => 150,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png'
+        ],
+        [
+            'name' => 'Eevee',
+            'type' => 'Normal',
+            'level' => 20,
+            'hp' => 75,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png'
+        ],
+        [
+            'name' => 'Psyduck',
+            'type' => 'Water',
+            'level' => 22,
+            'hp' => 85,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png'
+        ],
+        [
+            'name' => 'Snorlax',
+            'type' => 'Normal',
+            'level' => 30,
+            'hp' => 160,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'
+        ],
+        [
+            'name' => 'Dragonite',
+            'type' => 'Dragon/Flying',
+            'level' => 55,
+            'hp' => 200,
+            'img_url' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png'
+        ]
+    ];
+
+
+    return view(
+        'firstpage',
+        compact("titolo", "pokemonArray")
+    );
+})->name('firstpage');
+// Rotta per la seconda pagina
+Route::get('/secondpage', function () {
+    $titolo = 'Personaggi Pokemon';
+    $charactersArray = [
+        [
+            'name' => 'Ash Ketchum',
+            'role' => 'Allenatore di Pokémon',
+            'team' => 'Pikachu, Bulbasaur, Charizard, Squirtle, ecc.',
+        ],
+        [
+            'name' => 'Misty',
+            'role' => 'Capopalestra di Cerulean',
+            'team' => 'Starmie, Psyduck, Gyarados, Staryu, ecc.',
+        ],
+        [
+            'name' => 'Brock',
+            'role' => 'Capopalestra di Pewter',
+            'team' => 'Onix, Geodude, Crobat, Steelix, ecc.',
+        ],
+        [
+            'name' => 'Professor Oak',
+            'role' => 'Professore di Pokémon',
+            'team' => 'Nessuno (studioso)',
+        ],
+        [
+            'name' => 'Gary Oak',
+            'role' => 'Rivale di Ash',
+            'team' => 'Blastoise, Arcanine, Nidoking, Umbreon, ecc.',
+        ],
+        [
+            'name' => 'Jessie',
+            'role' => 'Membro del Team Rocket',
+            'team' => 'Wobbuffet, Seviper, Dustox, ecc.',
+        ],
+        [
+            'name' => 'James',
+            'role' => 'Membro del Team Rocket',
+            'team' => 'Weezing, Victreebel, Cacnea, Chimecho, ecc.',
+        ],
+        [
+            'name' => 'Giovanni',
+            'role' => 'Capo del Team Rocket',
+            'team' => 'Persian, Rhyperior, Nidoking, Kangaskhan, ecc.',
+        ],
+        [
+            'name' => 'Dawn',
+            'role' => 'Allenatrice di Pokémon e Coordinatrice',
+            'team' => 'Piplup, Buneary, Pachirisu, Mamoswine, ecc.',
+        ],
+        [
+            'name' => 'Cynthia',
+            'role' => 'Campionessa della Lega Pokémon di Sinnoh',
+            'team' => 'Garchomp, Lucario, Togekiss, Spiritomb, ecc.',
+        ]
+    ];
+
+
+    return view(
+        'secondpage',
+        compact("titolo", "charactersArray")
+    );
+})->name('secondpage');
